6bd473666b0028ddf4e2eb1732c08f23249af12d
RossanaPetrar <rossana.petrar@gmail.com>
2022-06-24 10:15:59 +0300

    Installed SASS and added basic functionality of game - choices and winner

--- app.js
+++ app.js
@@ -10,31 +10,77 @@ function randomOption() {
   return gameOptions[Math.floor(Math.random() * gameOptions.length)];
 }
 
-function pcChoice() {
-  let pcOption = randomOption();
-  if (pcOption === options.ROCK) {
-    console.log("Computer chose:", options.ROCK);
-  } else if (pcOption === options.PAPER) {
-    console.log("Computer chose:", options.PAPER);
-  } else if (pcOption === options.SCISSORS) {
-    console.log("Computer chose:", options.SCISSORS);
-  }
-}
-
+// USER CHOICE
 function userChoice() {
   let userOption = randomOption();
   if (userOption === options.ROCK) {
-    console.log("User chose:", options.ROCK);
+    return options.ROCK;
   } else if (userOption === options.PAPER) {
-    console.log("User chose:", options.PAPER);
+    return options.PAPER;
   } else if (userOption === options.SCISSORS) {
-    console.log("User chose:", options.SCISSORS);
+    return options.SCISSORS;
+  }
+}
+
+// COMPUTER CHOICE
+function pcChoice() {
+  let pcOption = randomOption();
+  if (pcOption === options.ROCK) {
+    return options.ROCK;
+  } else if (pcOption === options.PAPER) {
+    return options.PAPER;
+  } else if (pcOption === options.SCISSORS) {
+    return options.SCISSORS;
   }
 }
 
 const playButton = document.querySelector(".play-button");
+const mainEl = document.querySelector("main");
 
+// RENDER GAME
+function renderGame() {
+  let userResult = userChoice();
+  let pcResult = pcChoice();
+
+  function winner() {
+    if (userResult === pcResult) {
+      return "Draw!";
+    } else if (
+      (userResult === options.ROCK && pcResult === options.SCISSORS) ||
+      (userResult === options.PAPER && pcResult === options.ROCK) ||
+      (userResult === options.SCISSORS && pcResult === options.PAPER)
+    ) {
+      return "User wins!";
+    } else if (
+      (userResult === options.ROCK && pcResult === options.PAPER) ||
+      (userResult === options.PAPER && pcResult === options.SCISSORS) ||
+      (userResult === options.SCISSORS && pcResult === options.ROCK)
+    ) {
+      return "Computer wins!";
+    }
+  }
+
+  let gameResult = winner();
+
+  mainEl.innerHTML = `
+  <div class="container d-flex justify-content-center">
+  <section class="p-3 m-5 text-center player">
+    <h3>User chose:</h3>
+    <p class="gameOption">${userResult}</p>
+  </section>
+  <section class="p-3 m-5 text-center player">
+    <h3>Computer chose:</h3>
+    <p class="gameOption">${pcResult}</p>
+  </section>
+</div>
+<div class="container text-center">
+  <section class="m-3">
+    <h3>${gameResult}</h3>
+  </section>
+</div>`;
+}
+
+// PLAY BUTTON EVENT
 playButton.addEventListener("click", function play() {
-  pcChoice();
-  userChoice();
+  renderGame();
 });
